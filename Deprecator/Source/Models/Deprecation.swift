590cb6119d2ce080752be13d1a556a3ca1044d8e
Francisco Soares <francisco.ssn94@gmail.com>
2019-10-10 16:55:50 -0300

    [Internal] Add completion handler to present(in:language:)
    Allow client to have its own completion handler when presenting Deprecation alert

--- Deprecator/Source/Models/Deprecation.swift
+++ Deprecator/Source/Models/Deprecation.swift
@@ -40,7 +40,7 @@ public struct Deprecation: Decodable
     
     // MARK: Alerts
     
-    public func present(in viewController: UIViewController, language: String? = nil)
+    public func present(in viewController: UIViewController, language: String? = nil, completion: (() -> Void)? = nil)
     {
         var languageStrings = self.defaultLanguageStrings
         if let unwrappedLanguage = language,
@@ -66,7 +66,7 @@ public struct Deprecation: Decodable
         }
         
         // Present
-        viewController.present(alertController, animated: true, completion: nil)
+        viewController.present(alertController, animated: true, completion: completion)
     }
 }
 
